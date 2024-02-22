5f67070b135b81a748cbed401acf9659ebe92a47
toantran292 <tranthaitoan998@gmail.com>
2024-02-22 08:24:14 +0700

    contacts

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const contactRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactRouter);
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application" });
 });
