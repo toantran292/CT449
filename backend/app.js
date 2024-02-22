2de7cfb8e09e95fa6a1142a2d45b207234a31dfc
toantran292 <tranthaitoan998@gmail.com>
2024-02-22 09:12:15 +0700

    Handle Error

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const ApiError = require("./app/api-error");
 const contactRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactRouter);
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application" });
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;
