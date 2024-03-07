e678dc595ff40feeccddeef8280fc535f2dd2d56
toantran292 <tranthaitoan998@gmail.com>
2024-03-07 10:32:31 +0700

    Implement hanlder

--- backend/app/config/index.js
+++ backend/app/config/index.js
@@ -2,6 +2,9 @@ const config = {
   app: {
     port: process.env.PORT || 3000,
   },
+  db: {
+    uri: process.env.MONGODB_URI || "mongodb+srv://tranthaitoan999:29022004@ct449.m6vo3ck.mongodb.net/contactbook?retryWrites=true&w=majority&appName=CT449"
+    }
 };
 
 module.exports = config;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+async function startServer() {
+  try {
+    await MongoDB.connect(config.db.uri);
+    console.log("Connected to the db!")
+
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err){
+    console.log("Cannot connect to the db!\n >>> ", err)
+    process.exit()
+  }
+}
+
+startServer();
